16f8da7e4c19e2f0e90a11b0b06553b733952c1e
JorickLania <j.f.lania@student.rug.nl>
2026-05-27 14:21:31 +0200

    Updated get_current_weather with default params, human-readable printing.

--- weather.py
+++ weather.py
@@ -2,21 +2,36 @@
 import requests
 import os
 from dotenv import load_dotenv
+from pprint import pprint
 
 ## Load environment variables
 load_dotenv()
 
 ## Get-Weather-Function
-def get_current_weather():
+def get_current_weather(show_request=False, units='metric'):
+    '''Function requests weather data from OpenWeather.org based on user input city.
+       Input:
+       show_request    bool    : If True, prints full request url back to user. Default is False.
+       units           string  : Units of the returned weather data. Options are 'metric', 'imperial'. Default is 'metric'.
+    '''
     print("\n*** Let's take a look at the current weather! ***\n")
 
     city = input("\n Please specify a city: \n")
 
-    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units={units}'
+    if show_request is True:
+        print("Sending request to OpenWeather with request_url \n", request_url)
 
-    #print("Sending request to OpenWeather with request_url \n", request_url)
+    if units=='metric':
+        T_unit = 'C'
+    elif units=='imperial':
+        T_unit = 'F'    
 
     weather_data = requests.get(request_url).json()
-    print(weather_data)
+    print(f'Current Weather for {weather_data["name"]}:')
+    print(f'Current temperature: {weather_data["main"]["temp"]} {T_unit}, feels like {weather_data["main"]["feels_like"]} {T_unit}')
+    print(f'Current humidity: {weather_data["main"]["humidity"]}')
+    print(f'Weather info for today: {weather_data["weather"][0]["description"]}.')
 
-get_current_weather()
+if __name__ == "__main__":
+    get_current_weather()
